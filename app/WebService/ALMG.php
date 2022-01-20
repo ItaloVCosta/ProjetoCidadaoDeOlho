deb5a94aa16a48e7b782a0a7774ffbf66f1bdeca
Italo Costa <italo.costa99@gmail.com>
2022-01-20 17:35:20 -0300

    ALMG criado e formatado

--- app/WebService/ALMG.php
+++ app/WebService/ALMG.php
@@ -4,78 +4,70 @@ namespace app\WebService;
 
 class ALMG{
 
+    /**
+     * Envia a url necessaria para consultar os deputados
+     *
+     * @return array
+     */
+    public static function consultarDeputado()
+    { 
+        $url='http://dadosabertos.almg.gov.br/ws/legislaturas/19/deputados/situacao/1?formato=json';
 
-    public static function consultarDeputado(){
-         
-        $curl = curl_init();
-
-        // Confg do curl
-        curl_setopt_array($curl,[
-            CURLOPT_URL => 'http://dadosabertos.almg.gov.br/ws/legislaturas/19/deputados/situacao/1?formato=json',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => 'GET'
-        ]);
-
-        // Response
-        $response = curl_exec($curl);
-
-        // Fechando a conexão
-        curl_close($curl);
-
-        //print_r($response); 
-
-        $array = json_decode($response,true);
-
-        return $array;
+        return ALMG::solicitaApiALMG($url);
     }
 
+    /**
+     * Envia a url necessaria para consultar os dados do deputado
+     *
+     * @param  integer $id
+     * @return array
+     */
     public static function consultarDadoDeputado($id)
     {
-                 
-        $curl = curl_init();
+        $url='http://dadosabertos.almg.gov.br/ws/deputados/'.$id.'?formato=json';
 
-        // Confg do curl
-        curl_setopt_array($curl,[
-            CURLOPT_URL => 'http://dadosabertos.almg.gov.br/ws/deputados/'.$id.'?formato=json',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CUSTOMREQUEST => 'GET'
-        ]);
-
-        // Response
-        $response = curl_exec($curl);
-
-        // Fechando a conexão
-        curl_close($curl);
-
-        //print_r($response); 
-
-        $array = json_decode($response,true);
-
-        return $array;
+        return ALMG::solicitaApiALMG($url);
     }
 
-    
+    /**
+    * Envia a url necessaria para consultar os dados da verba indenizatoria
+    *
+    * @param integer $id
+    * @param string  $mes
+    * @return array 
+    */
     public static function consultarDadoVerbaIndenizatoria($id,$mes)
+    {
+        $url='http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/legislatura_atual/deputados/'.$id.'/2019/'.$mes.'?formato=json';
+
+        return ALMG::solicitaApiALMG($url);  
+    }
+
+    /**
+     * Solicita os dados para API da ALMG
+     *
+     * @param  string $url
+     * @return array
+     */
+    public static function solicitaApiALMG($url)
     {
         $curl = curl_init();
 
         // Confg do curl
         curl_setopt_array($curl,[
-            CURLOPT_URL => 'http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/legislatura_atual/deputados/'.$id.'/2019/'.$mes.'?formato=json',
+            CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_CUSTOMREQUEST => 'GET'
         ]);
 
-        // Response
-        $response = curl_exec($curl);
+        // Resposta
+        $respostaApi = curl_exec($curl);
 
         // Fechando a conexão
         curl_close($curl);
 
-        //print_r($response); 
+        $respostaApiArray = json_decode($respostaApi,true);
 
-        $array = json_decode($response,true);
-
-        return $array;  
-    }
+        return $respostaApiArray;
+    } 
 }
--- database/migrations/2022_01_20_171809_create_verbas_indenizatorias_table.php
+++ database/migrations/2022_01_20_171809_create_verbas_indenizatorias_table.php
@@ -13,7 +13,8 @@ class CreateVerbasIndenizatoriasTable extends Migration
      */
     public function up()
     {
-        Schema::create('verba_indenizatorias', function (Blueprint $table) {
+        Schema::create('verba_indenizatorias', function (Blueprint $table) 
+        {
             $table->id();
             $table->string('deputado_ids');
             $table->string('mes');
